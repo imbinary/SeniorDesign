00ea32328c2797db195d1d9ca443a75519e471f3
Chris Orem <chrisorem@gmail.com>
2015-08-12 17:18:21 -0400

    nmea checksum and string generation

--- GPS_task.h
+++ GPS_task.h
@@ -64,7 +64,7 @@ extern xSemaphoreHandle g_gpsUARTSemaphore;
 extern uint32_t GPSTaskInit(void);
 void GPSparse(char *gpsString);
 int8_t nmea_validateChecksum(char *strPtr);
-uint8_t nmea_generateChecksum(char *strPtr);
+const char * nmea_generateChecksum(char *strPtr, char *dstStr);
 //*****************************************************************************
 //
 // Forward declarations for command-line operations.
