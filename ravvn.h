bfd63b14b54df7379d75d3c83fd43d4a6575752f
Chris Orem <chrisorem@gmail.com>
2015-08-26 10:59:37 -1200

    .h

---
+++ ravvn.h
@@ -0,0 +1,40 @@
+/*
+ * ravvn.h
+ *
+ *  Created on: Aug 26, 2015
+ *      Author: Chris
+ */
+
+#ifndef RAVVN_H_
+#define RAVVN_H_
+
+//*****************************************************************************
+//
+// Structure to hold the BSM data and provide access to other tasks.
+//
+//*****************************************************************************
+typedef struct rBSMDataStruct
+{
+	float latitiude;
+	float longitude;
+	int16_t elevation;
+	float pAccuracy;
+	float speed;
+	uint16_t heading;
+	uint16_t steeringAngle;
+	float longAccel;
+	float latAccel;
+	float vertAccel;
+	float yawRate;
+	bool brake;
+	uint16_t length;
+	uint16_t width;
+    float timeStamp;
+
+} rBSMData_t;
+
+extern rBSMData_t g_rBSMData;
+
+const int  DTYPE = 1;    // 1 for vehicle, 0 for infrastructure
+
+#endif /* RAVVN_H_ */
